8d7b891dc99d6e2673620b67969171f8c43e81c1
ibkjy <t0mat09876C>
2021-03-05 15:57:16 +0800

    commit app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import UserInfo from "./components/UserInfo";
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import "./App.css";
+import logo from '../src/dbslogo.png';
 
 function App() {
   return (
-    <div className="App">
-      <UserInfo />
+    <div className='App'>
+      <header className='App-header'>
+        <img src = {logo} alt = "Logo" />
+        <h1 className='App-title'>TechTrek</h1>
+
+        <div id ="content">
+          <img src ="userblack.png"/>
+        </div>
+        
+      </header>
+
+      <div class="topnav">
+      <a href="#">Wallet Balance</a>
+      <a href="#">Buy & Sell</a>
+      <a href="#">Past Transactions</a>
+      </div>
+
+      {/* Specifies components to handle specific routes */}
+      <Router>
+        <div>
+          {/* This line specifies that if the browser URL matches /,
+          the ArticleListing component should be rendered in this place. */}
+          <Route exact path='/' component={ArticleListing} />
+          {/* Paths can contain parameters, for example, path="/post/:slug",
+          which are delivered to the target component through react props. */}
+          <Route path='/post/:slug' component={ArticleView} />
+        </div>
+      </Router>
     </div>
   );
 }
