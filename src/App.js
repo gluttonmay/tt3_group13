8aee9283331b27151a519df916a2aeee4f9c0ae8
Ting Xuan <tohtingxuan@gmail.com>
2021-03-05 16:39:17 +0800

    fix to error info

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
 import WalletBalance from "./Components/WalletBalance"
 import Header from "./Components/Header"
 import './App.css';
 import UserInfo from "./components/UserInfo";
-=======
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import TransactionHistory from './components/TransactionHistory'
-
->>>>>>> origin/Ting-Xuan
+import TransactionHistory from "./components/TransactionHistory";
 
 function App() {
   
   return (
     <Router>
-<<<<<<< HEAD
     <div className="App">
       <Header />
       <WalletBalance />
-
+      <TransactionHistory />
       <UserInfo />
     </div>
-=======
-      <div>
-        <Route path='/history' exact component={TransactionHistory} />
-      </div>
->>>>>>> origin/Ting-Xuan
     </Router>
   );
 }
