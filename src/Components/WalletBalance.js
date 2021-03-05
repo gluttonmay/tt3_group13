d5c7c93c786b4da87a6fd63829dc1b32e9f12f87
gluttonmay <67217951+gluttonmay@users.noreply.github.com>
2021-03-05 11:53:01 +0800

    axios.post

--- src/Components/WalletBalance.js
+++ src/Components/WalletBalance.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 
 export default function WalletBalance() {
+
+    const[assetBalance, setAssetBalance] = useState ("")
+    const[cashBalance, setCashBalance] = useState("")
+
+    const fetchbal = () => {
+        axios.post("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance", {
+            assetBalance: assetBalance,
+            cashBalance: cashBalance
+        }, 
+        {headers: QNd2HPwfhv2bK2pNt4pfl79YaNoq7p0X7XeSPkKY})
+        .then((response) => {
+            console.log(response.data[0])
+            setAssetBalance(response.data[0].assetBalance)
+            setCashBalance(response.data[0].cashBalance)
+        }).catch((error) => {
+            windows.alert(error)
+        })
+    }
 
     return (
         <div>
@@ -14,7 +33,7 @@ export default function WalletBalance() {
                     Asset Balance: 
                 </div>
                 <div>
-                    Cash Balance: 
+                    Cash Balance: $
                 </div>
             </div>
             
