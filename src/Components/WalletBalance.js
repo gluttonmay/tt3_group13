e9c0aabd189f1c0bc9ff225e758c089ea8333d94
gluttonmay <67217951+gluttonmay@users.noreply.github.com>
2021-03-05 16:15:24 +0800

    commit

--- src/Components/WalletBalance.js
+++ src/Components/WalletBalance.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import "./WalletBalance.css"
-//import { ​IoPersonCircle }​ from "react-icons/io5";
 
 export default function WalletBalance() {
 
