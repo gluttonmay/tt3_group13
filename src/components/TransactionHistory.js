c66f33adb6fb14ff70e610a0d0cc7e866c9dd243
Ting Xuan <tohtingxuan@gmail.com>
2021-03-05 15:33:33 +0800

    transaction history css

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import "./TransactionHistory.css"
 
 function TransactionHistory(props)
 {
@@ -8,7 +9,6 @@ function TransactionHistory(props)
     useEffect(() =>
     {
         const headers = {"x-api-key": "QNd2HPwfhv2bK2pNt4pfl79YaNoq7p0X7XeSPkKY"};
-        //const body = {"username": "Group13", "password": "QZAesJtZdQQyAJX"};
         const accountKey = "cc9fd35f-3d59-4700-8d78-ad5405c767ad"
 
         axios.post("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view", {accountKey}, {
@@ -16,25 +16,28 @@ function TransactionHistory(props)
           }).then((response) => {
             console.log(response);
             console.log(response.data);
-            setTransactions(response.data.transactions);
+            setTransactions(response.data);
           });
 
     }, []);
 
-    const transactionItems = transactions.map((transaction) =>    <li>{transaction.orderType}</li>  );
-
     return(
-        <ul>{transactionItems}</ul>
-            /* {transactions.map((transaction) =>    <li>{number}</li>  );
+        <div>
             <h1>TransactionHistory</h1>
-            <p>TransactionId: {transaction.transactionId}</p>
-            <p>Order Type: {transaction.orderType}</p>
-            <p>Timestamp: {transaction.timestamp}</p>
-            <p>Asset Symbol: {transaction.assetSymbol}</p>
-            <p>Asset Amount: {transaction.assetAmount}</p>
-            <p>Asset Price: ${transaction.assetPrice}</p>
-            <p>Cash Amount: ${transaction.cashAmount}</p>
-        </> */
+            {transactions.map((transaction) => {
+                return (
+                    <div className='transaction'>
+                    <h3>TransactionId: {transaction.transactionId}</h3>
+                    <p>Order Type: {transaction.orderType}</p>
+                    <p>Timestamp: {transaction.timestamp}</p>
+                    <p>Asset Symbol: {transaction.assetSymbol}</p>
+                    <p>Asset Amount: {transaction.assetAmount}</p>
+                    <p>Asset Price: ${transaction.assetPrice}</p>
+                    <p>Cash Amount: ${transaction.cashAmount}</p>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 
