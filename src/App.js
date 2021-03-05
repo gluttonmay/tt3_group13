99f30ec5ba7d7cea6750d714b89ece44f8b1181e
gluttonmay <67217951+gluttonmay@users.noreply.github.com>
2021-03-05 11:25:19 +0800

    WalletBalance skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import WalletBalance from "./Components/WalletBalance"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      check
+      <WalletBalance />
     </div>
   );
 }
---
+++ src/Components/WalletBalance.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function WalletBalance() {
+
+    return (
+        <div>
+            <div>
+                <img src= "https://static.thenounproject.com/png/3201525-200.png"/>
+            </div>
+
+            <div>
+                <h2>Available Balance</h2>
+                <div>
+                    Asset Balance: 
+                </div>
+                <div>
+                    Cash Balance: 
+                </div>
+            </div>
+            
+        </div>
+    )
+}
