478339283fe38cce631c196f64240af0d39ea35e
murphytkh <murphytkh@email.com>
2021-03-05 16:15:00 +0800

    Revert "push"
    This reverts commit 420f55a13250995de05f8fa74624fb75f0f41b2d.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
-import WalletBalance from "./components/WalletBalance"
-import Header from "./components/Header"
-import UserInfo from "./components/UserInfo";
+import logo from './logo.svg';
 import './App.css';
+import UserInfo from "./components/UserInfo";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <WalletBalance />
-    
-      </div>
-    </Router>
+    <div className="App">
+      <UserInfo />
+    </div>
   );
 }
 
