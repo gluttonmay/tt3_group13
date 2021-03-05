58629c00f47434f5bbb7f0900b05345e15e51387
minghanzz <minghan.limit@gmail.com>
2021-03-05 15:56:49 +0800

    css

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import './Login.css';
+import jwt from 'jsonwebtoken';
 
 
 // Password: QZAesJtZdQQyAJX
@@ -9,7 +11,7 @@ export default function Login() {
 
     // write this to use history.push later on
     const history = useHistory();
-// changes
+
     // get inputs from the input fields to send to API for validation
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -36,11 +38,14 @@ export default function Login() {
             if(!response.status===200) {
                 setLoginStatus(false);
             } else {
-                localStorage.setItem('token', response.data);
+                // const id = response[0];
+                // const token = jwt.sign({id}, 'secret', {
+                //     expiresIn: 500
+                // });
+                localStorage.setItem('token', response.data[0]);
                 setLoginStatus(true);
                 history.push('/account');
             }
-            console.log(loginStatus);
         });
     };
 
@@ -53,24 +58,35 @@ export default function Login() {
     }, [])
 
     return (
-        <div>
-            <input 
-                type='text' 
-                placeholder='username'
-                onChange={(e) =>{
-                    setUsername(e.target.value);
-                }} 
-                required
-            /><br></br>
-            <input 
-                type='password' 
-                placeholder='password'
-                onChange={(e) =>{
-                    setPassword(e.target.value);
-                }} 
-                required
-            /><br></br>
-            <button onClick={login}>Login</button>
+        <div
+            className = 'div1' 
+            style={{ 
+                backgroundImage: `url(${process.env.PUBLIC_URL + 'https://images.unsplash.com/photo-1542114740389-9b46fb1e5be7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1778&q=80'})` ,
+                backgroundRepeat: 'no-repeat'
+            }}
+        >
+            <div className="div2">
+                <form className='input-fields'>
+                    <h1>Welcome</h1>
+                    <input 
+                        type="text" 
+                        placeholder="Username" 
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }} 
+                        required
+                    /><br></br>
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}                             
+                        required
+                    /><br></br>
+                    <button className="submit" onClick={login}>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
