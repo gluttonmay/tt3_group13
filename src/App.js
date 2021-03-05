6e95af641481da518234f1d6522d3c7756ea8e1b
Ting Xuan <tohtingxuan@gmail.com>
2021-03-05 17:11:12 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
-import WalletBalance from "./Components/WalletBalance"
-import Header from "./Components/Header"
+import WalletBalance from "./components/WalletBalance"
+import Header from "./components/Header"
 import './App.css';
 import UserInfo from "./components/UserInfo";
 import TransactionHistory from "./components/TransactionHistory";
