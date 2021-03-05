bd482f5a5a4027340393091ef6c2a1450bcb7b85
minghanzz <minghan.limit@gmail.com>
2021-03-05 11:08:11 +0800

    basic skeleton

---
+++ src/Component/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function Login() {
+
+    
+
+    return (
+        <div>
+            <input 
+                type='text' 
+                placeholder='username'/>
+            <input 
+                type='password' 
+                placeholder='password'/>
+        </div>
+    )
+}
