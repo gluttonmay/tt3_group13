4697a70670c989b16e47f1da494e0846053cfa96
minghanzz <minghan.limit@gmail.com>
2021-03-05 11:14:28 +0800

    setting state

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function Login() {
 
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <div>
             <input 
                 type='text' 
-                placeholder='username'/>
+                placeholder='username'
+                onChange={(e) =>{
+                    setUsername(e.target.value);
+                }} 
+                required
+            />
             <input 
                 type='password' 
-                placeholder='password'/>
+                placeholder='password'
+                onChange={(e) =>{
+                    setUsername(e.target.value);
+                }} 
+                required
+            />
+            <button>Login</button>
         </div>
     )
 }
