1f7fdb7f071a59c6c79fac1545cbbbfd87bc7566
minghanzz <minghan.limit@gmail.com>
2021-03-05 13:07:45 +0800

    login (can fetch API)

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
+// Password: QZAesJtZdQQyAJX
+
 export default function Login() {
 
     const [username, setUsername] = useState('');
@@ -8,14 +11,29 @@ export default function Login() {
 
     const [loginStatus, setLoginStatus] = useState(false);
 
+    var postData = {
+        'username': username,
+        'password': password,
+    };
+
+    let axiosConfig = {
+        headers: {
+                'x-api-key': 'QNd2HPwfhv2bK2pNt4pfl79YaNoq7p0X7XeSPkKY',
+        }
+    };
+
     const login = () => {
-        axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login', {
-            username: username,
-            password: password,
-        }, {
-            'x-api-key': 'QNd2HPwfhv2bK2pNt4pfl79YaNoq7p0X7XeSPkKY' 
-        }).then((res) => {
-            console.log(res);
+        axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login', postData, axiosConfig)
+        .then((response) => {
+            console.log(response);
+            if(!response.status===200) {
+                setLoginStatus(false);
+            } else {
+                localStorage.setItem('token', response.data);
+                setLoginStatus(true);
+
+            }
+            console.log(loginStatus);
         }
     );
 };
@@ -35,7 +53,7 @@ export default function Login() {
                 type='password' 
                 placeholder='password'
                 onChange={(e) =>{
-                    setUsername(e.target.value);
+                    setPassword(e.target.value);
                 }} 
                 required
             />
