ed28c54542c48d0ebe14cdf6eac76c476662d1a1
murphytkh <murphytkh@email.com>
2021-03-05 13:53:51 +0800

    commented out stuff, removed accountkey

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
+//import {BrowserRouter, Route, useHistory, useLocation} from "react-router-dom";
 import axios from "axios";
 
 function UserInfo(props)
 {
-    const [accountKey, setAccountKey] = useState("key123");
     const [firstName, setFirstName] = useState("first");
     const [lastName, setLastName] = useState("last");
     const [nric, setNric] = useState("S1234567C");
@@ -13,6 +13,7 @@ function UserInfo(props)
 
     useEffect(() =>
     {
+        // get user info from api
         const headers = {"x-api-key": "QNd2HPwfhv2bK2pNt4pfl79YaNoq7p0X7XeSPkKY"};
         const body = {"username": "Group13", "password": "QZAesJtZdQQyAJX"};
 
@@ -20,21 +21,53 @@ function UserInfo(props)
             headers: headers
           }).then((response) => {
             console.log(response);
-            console.log(response.data);
-            setAccountKey(response.data.accountKey);
-            setAddress(response.data.address);
-            setEmail(response.data.email);
-            setFirstName(response.data.firstName);
-            setLastName(response.data.lastName);
-            setNric(response.data.nric);
-            setPhoneNumber(response.data.phoneNumber);
+            if (response.status === 200)
+            {
+                setAddress(response.data.address);
+                setEmail(response.data.email);
+                setFirstName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setNric(response.data.nric);
+                setPhoneNumber(response.data.phoneNumber);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
           });
 
     }, []);
 
+    function handleBalanceClick()
+    {
+        //history.push("/balance");
+        console.log("balance");
+    }
+
+    function handleMarketClick()
+    {
+        //history.push("/market");
+        console.log("market");
+    }
+
+    function handleHistory()
+    {
+        //history.push("/history");
+        console.log("history");
+    }
+
+    function handleLogout()
+    {
+        //history.push("/login");
+        console.log("logout");
+    }
+
     return(
-        <div style = {{float: "left", }}>
-            <h1>{accountKey}</h1>
+        <div style = {{float: "left"}}>
+            <button type = "button" onClick = {handleBalanceClick}>Balance</button>
+            <button type = "button" onClick = {handleMarketClick}>Market</button>
+            <button type = "button" onClick = {handleHistory}>History</button>
+            <button type = "button" onClick = {handleLogout}>Logout</button>
+
             <h1>{firstName}</h1>
             <h1>{lastName}</h1>
             <h1>{nric}</h1>
