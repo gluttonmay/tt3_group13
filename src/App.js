7976a7a164eca0062619288a4690364d03ff9df7
Ting Xuan <tohtingxuan@gmail.com>
2021-03-05 11:58:28 +0800

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import TransactionHistory from './components/TransactionHistory'
+
 
 function App() {
+  const [transactions, setTransactions] = useState([])
+  const accountKey = ""
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const transactionsFromAPI = await fetchTransactionHist(accountKey)
+      setTransactions(transactionsFromAPI)
+    }
+
+    getTasks()
+  }, [])
+
+  //Fetch transaction history
+  const fetchTransactionHist = async (accountKey) => {
+    const res = await fetch(`https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view/${accountKey}`)
+    const data = await res.json()
+
+    return data
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 }
